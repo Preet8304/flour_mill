1e9bf01788d7a7ada692954d00792526d252471c
SHREYANSH <shreyanshchichi@gmail.com>
2024-09-19 11:24:47 +0530

    Login and Signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 import 'Screens/HomePage.dart';
 
-
 void main() {
-  runApp(MyApp(
-
-
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Mill to Door",
+      title: " Mill to Door",
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white
+        scaffoldBackgroundColor: const Color.fromARGB(10, 174, 56, 200),
       ),
       debugShowCheckedModeBanner: false,
       routes: {
-        "/": (context) =>   const HomePage(),
-      },
+        "/":(context)=> HomePage()},
     );
   }
 }
